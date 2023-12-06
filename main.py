f3539366eb7d029e3d01b0235c42a50163f97e89
eric_waller <eric.waller@vtsd.com>
2023-12-06 07:58:14 -0500

    ev

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import turtle as trtl
 box = trtl.Turtle()
 lines = trtl.Turtle()
 wn = trtl.Screen()
+lines.speed(0)
 
 # Setup Screen
 def setupScreen():
@@ -31,6 +32,16 @@ def setupBox():
 # Code for 80 point version goes here
 def v80():
    lines.goto(-490, -300)
+   start_x = -490
+   start_y = -300
+   end_x = 490
+   end_y = -290
+   for line in range(32):
+       lines.goto(end_x, end_y)
+       lines.goto(start_x, start_y)
+       start_x += 30
+       end_y += 20
+       lines.goto(start_x, start_y)
 
 
 
@@ -39,6 +50,17 @@ def v80():
 def v90():
     # Calling the 80 point function - don't copy-paste from earlier method!!
     v80()
+    lines.goto(490, -300)
+    start_x = 490
+    start_y = -300
+    end_x = -490
+    end_y = -290
+    for line in range(32):
+        lines.goto(end_x, end_y)
+        lines.goto(start_x, start_y)
+        start_x -= 30
+        end_y += 20
+        lines.goto(start_x, start_y)
 
 
 
@@ -47,6 +69,28 @@ def v90():
 def v100():
     # Calling the 90 point function - don't copy-paste from earlier method!!
     v90()
+    lines.goto(-490, 330)
+    start_x = -490
+    start_y = 330
+    end_x = 490
+    end_y = 320
+    for line in range(32):
+        lines.goto(end_x, end_y)
+        lines.goto(start_x, start_y)
+        start_x += 30
+        end_y -= 20
+        lines.goto(start_x, start_y)
+    lines.goto(490, 330)
+    start_x = 490
+    start_y = 330
+    end_x = -490
+    end_y = 320
+    for line in range(32):
+        lines.goto(end_x, end_y)
+        lines.goto(start_x, start_y)
+        start_x -= 30
+        end_y -= 20
+        lines.goto(start_x, start_y)
 
 
 
@@ -55,11 +99,22 @@ def v100():
 def v110():
     # Calling the 100 point function - don't copy-paste from earlier method!!
     v100()
+    lines.goto(-290, -100)
+    start_x = -290
+    start_y = -100
+    end_x = 290
+    end_y = -90
+    for line in range(32):
+        lines.goto(end_x, end_y)
+        lines.goto(start_x, start_y)
+        start_x += 18
+        end_y += 7
+        lines.goto(start_x, start_y)
 
 
 setupScreen()
 setupBox()
-
+v110()
 
 
 
